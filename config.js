c88200bf3861973f6ae230bcc3363a587c2bdba5
1mbadatcoding <zsledge01@gmail.com>
2025-09-05 13:12:48 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
     apiKey: "AIzaSyC_7zPSVxGRs937xcx56ORkQpjGs8lvrVw",
     authDomain: "the-secret-notes.firebaseapp.com",
     projectId: "the-secret-notes",
-    storageBucket: "the-secret-notes.firebasestorage.app",
+    storageBucket: "the-secret-notes.appspot.com",
     messagingSenderId: "949281009499",
     appId: "1:949281009499:web:d1397ff5b03ed98848347e"
   };
